6ab63f4d096a21c588b42ed62f04675a308827c2
Alec Hall <alechall1990@yahoo.com>
2019-01-27 12:29:39 -0600

    added name,pass,verpass and email functionality

--- main.py
+++ main.py
@@ -4,20 +4,45 @@ import cgi
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-error = "there is and error"
-
 @app.route("/")
 def index():
     
     return render_template('home.html')
 
 @app.route("/user_signup", methods=['POST'])
-def enter_name():
+def errors():
+    name_error = ''
+    pass_error = ''
+    ver_pass_error = ''
+    email_error = ''
+
     username = request.form["username"]
+    password = request.form["password"]
+    ver_password = request.form["ver_password"]
+    email = request.form["email"]
 
     if len(username) == 0 :
-        return render_template('home.html', error = error)
-        print("error")
+        name_error = "You did not enter a valid username"
+
+    if len(password) == 0 :
+        pass_error = "You did not enter a valid password"
+    
+    if ver_password != password or len(ver_password) == 0 :
+        ver_pass_error = "Your passwords do not match"
+
+    for let in email:
+        if let == '':
+            email_error = ''
+            break
+
+        elif let == "@":
+            email_error = ''
+            break
+            
+        else:
+            email_error = "Please enter a valid email"
+    
         
-    return render_template('home.html')
+        
+    return render_template('home.html', name_error=name_error, pass_error=pass_error, ver_pass_error = ver_pass_error, email_error = email_error)
 app.run()
