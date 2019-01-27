fbb5aba03cd3e59e9b06549f4e22eb03a2750cf5
Alec Hall <alechall1990@yahoo.com>
2019-01-27 13:32:00 -0600

    wrote python functions

--- main.py
+++ main.py
@@ -5,25 +5,8 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 def username_func():
-    
-
-@app.route("/")
-def index():
-    
-    return render_template('home.html')
-
-@app.route("/user_signup", methods=['POST'])
-def errors():
-
+    username = request.form['username']
     name_error = ''
-    pass_error = ''
-    ver_pass_error = ''
-    email_error = ''
-
-    username = request.form["username"]
-    password = request.form["password"]
-    ver_password = request.form["ver_password"]
-    email = request.form["email"]
 
     if len(username) < 3 or len(username) > 20 :
         name_error = "You did not enter a valid username (must be 3-20 characters)"
@@ -31,17 +14,35 @@ def errors():
     for let in username:
         if let == ' ':
             name_error = "You did not enter a valid username (may not contain spaces)"
-            
+
+    return name_error
+
+def password_func():
+    password = request.form['password']
+    pass_error = ''
+
+    if len(password) < 3 or len(password) > 20 :
+            pass_error = "You did not enter a valid password (must be 3-20 characters)"
         
-    if len(password) == 0 :
-        pass_error = "You did not enter a valid password"
-    
     for char in password:
         if char == ' ':
-            pass_error = "no spaces"
-    
-    if ver_password != password or len(ver_password) == 0 :
+            pass_error = "You did not enter a valid password (may not contain spaces)"
+
+    return pass_error
+
+def ver_pass_func():
+    ver_password = request.form['ver_password']
+    password = request.form['password']
+    ver_pass_error = ''
+
+    if ver_password != password or len(ver_password) == 0:
         ver_pass_error = "Your passwords do not match"
+    
+    return ver_pass_error
+
+def email_func():
+    email = request.form['email']
+    email_error = ''
 
     for let in email:
         if let == '':
@@ -54,6 +55,24 @@ def errors():
 
         else:
             email_error = "Please enter a valid email"
+
+    return email_error
+
+    
+
+@app.route("/")
+def index():
+    return render_template('home.html')
+
+@app.route("/user_signup", methods=['POST'])
+def errors():
+    name_error = username_func()
+    pass_error = password_func()
+    ver_pass_error = ver_pass_func()
+    email_error = email_func()
+                 
+
+   
     
         
         
